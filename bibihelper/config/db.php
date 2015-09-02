547bb3e4605a548a3eccee2a77ec46c0d77829c2
Leon <return_to_castle@mainl.ru>
2015-09-02 23:54:35 +0600

    Yii2

---
+++ bibihelper/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql.hostinger.ru;dbname=u336579318_bbhls',
+    'username' => 'u336579318_userb',
+    'password' => 'bbhls336579318',
+    'charset' => 'utf8',
+];
