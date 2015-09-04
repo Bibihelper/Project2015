aa259a0164f25ef08eea9a6dc89a7f09a14392cd
Leon <return_to_castle@mainl.ru>
2015-09-05 01:41:11 +0600

    db connection for server and localhost.

--- bibihelper/config/db.php
+++ bibihelper/config/db.php
@@ -1,9 +1,19 @@
-﻿<?php
+<?php
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql.hostinger.ru;dbname=u336579318_bbhls',
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=bibihelper_stage',
+    'username' => 'galaxy',
+    'password' => 'zxcvbnm',
+    'charset' => 'utf8',
+];
+
+/*
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql.hostinger.ru;port=3306;dbname=u336579318_bbhls',
     'username' => 'u336579318_userb',
     'password' => 'bbhls336579318',
     'charset' => 'utf8',
 ];
+*/
