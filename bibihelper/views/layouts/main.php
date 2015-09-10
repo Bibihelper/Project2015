c552ddcd0065cb9dbe6dd25ad42d74f2f5d1a010
Leon <return_to_castle@mainl.ru>
2015-09-10 19:07:16 +0600

    Обновлен layout main.php.

--- bibihelper/views/layouts/main.php
+++ bibihelper/views/layouts/main.php
@@ -4,7 +4,9 @@
 /* @var $content string */
 
 use yii\helpers\Html;
+
 ?>
+
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
@@ -26,7 +28,95 @@ use yii\helpers\Html;
 </head>
 <body>
 <?php $this->beginBody() ?>
+
+<div class="container-fluid header1">
+    <div class="row">
+        <div class="container">
+            <div class="row menu">
+            <? if ($this->params['page'] == 'index'): ?>
+                <div class="menu__item"><a href="#" title="" data-toggle="modal" data-target="#private-room-entry" id="pre">Войти в личный кабинет</a></div>
+                <div class="menu__item menu__item_mrg-r"><a href="#" title="" data-toggle="modal" data-target="#register-company" id="reg">Добавить автосервис</a></div>
+                <div class="menu__item menu__item_mrg-r" style="display: none;"><a href="#" title="" data-toggle="modal" data-target="#restore-psw" id="rst">Восстановить пароль</a></div>
+            <? endif; ?>
+            <? if ($this->params['page'] == 'private-room'): ?>
+                <div class="menu__item"><a href="#" title="">Mymail@mail.ru</a></div>
+            <? endif; ?>
+            </div> <!-- /row -->
+        </div> 
+    </div> <!-- /row -->
+</div>
+
+<div class="container-fluid header2">
+    <div class="row">
+        <div class="container">                
+            <div class="row">
+            
+                <div class="logo">
+                    <p class="logo__bibi-helper logo__bibi_color logo__bibi_shift"><span>BiBi<span class="logo__bibi-helper logo__helper_color">Helper</span></span></p>
+                    <p class="logo__slogan">Удобный выбор автосервиса</p>                        
+                </div>
+                
+                <? if ($this->params['page'] == 'index'): ?>
+                    <div class="city">                                             
+                        <div class="btn-group">
+                            <button type="button" class="btn dropdown-toggle bibi-btn bibi-btn_city" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="bibi-btn-text" id="city__btn" data-city-id="0">Мой город</span>
+                                <span class="caret bibi-btn-caret"></span>
+                            </button>
+                            <ul class="dropdown-menu bibi-list bibi-list_city">
+                                <li data-city-id="1" data-city-coords="{latitude: 55.75368981, longitude: 37.61855388}"><a href="#" title="">Москва</a></li>
+                                <li data-city-id="2" data-city-coords="{latitude: 59.93291880, longitude: 30.31607890}"><a href="#" title="">Санкт-Питербург</a></li>
+                                <li data-city-id="3" data-city-coords="{latitude: 55.01749779, longitude: 82.92943263}"><a href="#" title="">Новосибирск</a></li>
+                                <li data-city-id="4" data-city-coords="{latitude: 58.52212461, longitude: 31.27240443}"><a href="#" title="">Новгород</a></li>
+                                <li data-city-id="5" data-city-coords="{latitude: 43.12041367, longitude: 131.8851974}"><a href="#" title="">Владивосток</a></li>
+                            </ul>
+                        </div>                                                                                               
+                    </div>
+                <? endif; ?>
+                
+            </div> <!-- /row -->               
+        </div>     
+    </div> <!-- /row -->
+</div>
+
 <?= $content ?>
+
+<div class="pre-footer"></div>
+
+<div class="container-fluid footer">
+    <div class="row">
+        <div class="container">
+            <div class="row">
+                
+                <div class="f-menu">
+                    <div class="f-menu__col">
+                        <span><a href="#" title="">Для автосервисов</a></span>
+                        <span><a href="#" title="">Автовладельцам</a></span>
+                    </div>
+                    <div class="f-menu__col">
+                        <span><a href="#" title="">О проекте</a></span>
+                        <span><a href="#" title="">Обратная связь</a></span>                        
+                    </div>
+                </div>
+                
+                <div class="f-block">
+                    <div class="f-block__social">
+                        <script type="text/javascript" src="http://vk.com/js/api/share.js?92" charset="windows-1251"></script>
+                        <script type="text/javascript"><!--
+                            document.write(VK.Share.button(false,{type: "custom", text: "<img src=\"http://vk.com/images/share_32_eng.png\" width=\"32\" height=\"32\" />", eng: 1}));
+                        --></script>
+                    </div>
+                    <div class="f-block__copyright">
+                        <p><span class="copyright-sign">&nbsp;&nbsp;&nbsp;&nbsp;&copy;&nbsp;</span><span class="copyrighter">2015&nbsp;BiBiHelper</span></p>
+                    </div>
+                </div>
+                
+            </div> <!-- /row -->
+        </div>    
+    </div> <!-- /row -->
+    <span class="version">v.0.1</span>
+</div>
+
 <?php $this->endBody() ?>
 </body>
 </html>
